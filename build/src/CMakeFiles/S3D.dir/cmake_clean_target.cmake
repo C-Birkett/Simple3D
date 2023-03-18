4767605b1dccd28836c92a73a1eb751d7a21b1ab
C-Birkett <conanbirkett@gmail.com>
2023-03-18 20:37:17 +0000

    ...

--- build/src/CMakeFiles/S3D.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libS3D.a"
-)
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/conan/Work/Simple3D/src
-# Build directory: /home/conan/Work/Simple3D/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
